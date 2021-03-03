7fea5d521a1c6ea7e90c997917b5b6b27a3d3a3b
Martinez_Julian <59780954+dead-skin-mask@users.noreply.github.com>
2021-03-02 19:35:58 -0500

    Add files via upload

---
+++ ejercicio javaScript/ejercicio.js
@@ -0,0 +1,123 @@
+let menu = prompt(
+  "Elige uno de los siguientes animales para obtener informacion: \n 1. Vertebrados \n 2. Invertebrados \n 3. Herbivoros \n 4. Carnivoros"
+);
+if (menu !== "1" && menu !== "2" && menu !== "3" && menu !== "4") {
+  alert("Debe ser una opcion entre 1 y 4");
+} else {
+  if (menu === "1") {
+    prompt("1. Perros \n 2. gatos \n 3. elefantes \n 4. leones \n 5. jirafas");
+    if (menu === "1") {
+      prompt(
+        "El perro es uno de los animales domésticos más antiguos del mundo y el mejor amigo del hombre. Su domesticación comienza cuando apenas era un pariente salvaje de los actuales lobos "
+      );
+    }
+    if (menu === "2") {
+      prompt(
+        "Se trata de un mamífero de contextura pequeña, carnívoro y cuyos rasgos salientes son: cabeza redonda, lengua áspera, patas cortas, uñas importantes, filosas y retráctiles, abundante pelaje y más bien suave, y las pupilas se le dilatan para poder ver mejor a instancias de la oscuridad."
+      );
+    }
+    if (menu === "3") {
+      prompt(
+        "Son animales mamíferos, exclusivamente herbívoros cuya característica principal –además del gran tamaño– es su larga trompa y sus enormes orejas. Se organizan en manadas de unos 20 miembros, miden casi 4 metros los ejemplares más grandes y pueden llegar a pesar más de 6.000 kilos."
+      );
+    }
+    if (menu === "4") {
+      prompt(
+        "El león (de nombre científico Panthera leo) es un mamífero cuadrúpedo de la familia de los félidos, típico del África subsahariana y algunas regiones del Congo y de la India. Ocupa el lugar superior de la cadena alimenticia, pues se trata de uno de los mayores depredadores del planeta en la actualidad."
+      );
+    }
+    if (menu === "5") {
+      prompt(
+        "La jirafa es el animal terrestre más alto del mundo y uno de los protagonistas indiscutibles de la sabana africana. Esta peculiar especie puede llegar a medir 5 metros de altura y su nombre en latín Giraffa camelopardalis proviene de los romanos, que las consideraban una mezcla de camello y leopardo."
+      );
+    }   
+  }
+  if (menu === "2") {
+    prompt(
+      "1. Arañas \n 2. Libelulas \n 3. Luciernagas \n 4. Avispas \n 5.Medusas"
+    );
+    if (menu === "1") {
+      prompt(
+        "Las arañas son unos animales llamados artrópodos, que respiran aire, tienen ocho patas, y quelíceros modificados en colmillos capaces de inyectar veneno. Las arañas viven en todos los continentes excepto la Antártida."
+      );
+    }
+    if (menu === "2") {
+      prompt(
+        "Son paleópteros, es decir, insectos que no pueden plegar las alas sobre el abdomen. Se caracterizan por sus grandes ojos multifacetados, sus dos pares de fuertes alas transparentes y por su abdomen alargado. Se alimentan de mosquitos y otros pequeños insectos como moscas, abejas, mariposas y polillas."
+      );
+    }
+    if (menu === "3") {
+      prompt(
+        "Las luciérnagas son insectos muy conocidos, pero poca gente sabe que son en realidad escarabajos, miembros noctámbulos de la familia de los lampíridos. La mayoría de las luciérnagas tienen alas, lo que las distingue de otros insectos luminiscentes de la misma familia, conocidos comúnmente como gusanos de luz."
+      );
+    }
+    if (menu === "4") {
+      prompt(
+        "Las avispas son insectos voladores pertenecientes al orden Hymenoptera y al suborden Apocrita; presentes alrededor del mundo. Se diferencian de las abejas por la delgadísima “cintura” que separa el abdomen del tórax."
+      );
+    }
+    if (menu === "5") {
+      prompt(
+        "Las medusas pertenecen al filo Cnidaria, formado por un grupo de más de 10,000 especies de animales marinos. Tienen cuerpo gelatinoso (en inglés es llamado Jellyfish) y es conocido por varios nombres en diversas culturas: aguamala, malaguas, aguavivas o lágrimas de mar."
+      );
+    }
+  }
+  if (menu === "3") {
+    prompt("1. Caballo \n 2. Cabra \n3. Canguro \n4. Cebra \n5. Ciervo");
+    if (menu === "1") {
+      prompt(
+        "El caballo es un mamífero cuadrúpedo, perisodáctilo, perteneciente a la familia de los équidos. A la hembra de los caballos se la denomina yegua, a la cría de caballo-yegua, potros o potrillos en el caso de ser machos y potrancas si son hembras."
+      );
+    }
+    if (menu === "2") {
+      prompt(
+        "Cabra. (Del lat. capra). Mamífero rumiante doméstico, como de un metro de altura, ligero, esbelto, con pelo corto, áspero y a menudo rojizo, cuernos huecos, grandes, esquinados, nudosos y vueltos hacia atrás, un mechón de pelos largos colgante de la mandíbula inferior y cola muy corta."
+      );
+    }
+    if (menu === "3") {
+      prompt(
+        "Los canguros poseen grandes y poderosas patas traseras, grandes pies aptos para saltar, una cola larga y musculosa para mantener el equilibrio y una cabeza pequeña. Los canguros son herbívoros, alimentándose de pasto y raíces. ... Popularmente el canguro es conocido como el animal más representativo de Australia"
+      );
+    }
+    if (menu === "4") {
+      prompt(
+        "El concepto de cebra alude a un animal mamífero que, al igual que el caballo y el asno (también llamado burro), pertenece al género Equus. Las cebras se caracterizan por combinar rayas negras y blancas en su cuerpo."
+      );
+    }
+    if (menu === "5") {
+      prompt(
+        "El ciervo o venado es un animal esbelto, robusto, bien conformado y de porte majestuoso y altivo. Su corpulencia puede variar de modo considerable, siendo la hembra bastante más pequeña que el macho, del que difiere también por el color del manto. es muy tímido y miedoso, y no muy astuto ni inteligente."
+      );
+    }
+  }
+  if (menu === "4") {
+    prompt(
+      "1. Tigre \n 2. Pulpo \n 3. Ornitorrinco \n 4. Leon Marino \n5. Pinguino "
+    );
+    if (menu === "1") {
+      prompt(
+        "Tigre es el nombre común que reciben los integrantes de la especie Panthera tigris. Este animal mamífero, que está considerado como el felino más grande del planeta, se caracteriza por su pelaje amarillo con rayas negras en el lomo."
+      );
+    }
+    if (menu === "2") {
+      prompt(
+        "Los octópodos son animales marinos de la familia de los moluscos cefalópodos que carecen de esqueleto: ni interno ni externo. ... Con su cabeza de forma bulbosa, ocho potentes brazos provistos de tentáculos y dos ojos saltones, el pulpo es uno de los animales más peculiares que podemos encontrar en el fondo marino."
+      );
+    }
+    if (menu === "3") {
+      prompt(
+        "El ornitorrinco (Ornithorhynchus anatinus) es una especie de mamífero semiacuático endémico del este de Australia y de la isla de Tasmania. ... Es uno de los pocos mamíferos venenosos; los machos tienen un espolón en las patas posteriores que libera un veneno capaz de producir un dolor intenso a los humanos."
+      );
+    }
+    if (menu === "4") {
+      prompt(
+        "Los leones marinos comparten ciertas características con las focas, pero los primeros poseen oídos externos, su piel es gruesa, pelaje corto y muy voluminoso. Los machos son más grandes que las hembras, por lo regular hasta cinco veces más, lo que los convierte en uno de los animales con mayor dimorfismo sexual."
+      );
+    }
+    if (menu === "5") {
+      prompt(
+        "Los pingüinos son excelentes nadadores y buceadores. Pueden nadar a más de 40 kilómetros (25 millas) por hora. En el agua, los pingüinos utilizan sus alas en forma de paleta, como si fueran aletas. Tienen las patas palmeadas y el cuerpo en forma de torpedo"
+      );
+    }
+  }
+}
